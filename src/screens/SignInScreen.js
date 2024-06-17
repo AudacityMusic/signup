f375a3c3de9f41b4426fea500fc42e2c5c740cbd
Pramad712 <pmaddineedi@gmail.com>
2024-06-16 19:14:52 -0700

    Add back img and edit files

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import React, {useState} from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Linking, Pressable } from "react-native";
 
 export default function SignInScreen() {
   return (
-    // TODO
-    <Text>Sign In</Text>
+    <SafeAreaView>
+      <Pressable>
+        <Image source={require("./src/assets/back.png")} />
+      </Pressable>
+      <View style={styles.container}>
+        <Text style={styles.title}>Sign in</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  // TODO
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+  }
 });
