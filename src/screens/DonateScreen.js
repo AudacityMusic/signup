860f86bfac2f58e67450918b4a2e5f7268f10110
TenType <55125103+TenType@users.noreply.github.com>
2024-06-21 16:58:49 -0700

    Add template files

---
+++ src/screens/DonateScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+export default function DonateScreen() {
+  return (
+    // TODO
+    <Text>Donate</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  // TODO
+});
---
+++ src/screens/SelectProgramScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+export default function SelectProgramScreen() {
+  return (
+    // TODO
+    <Text>Select a Program</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  // TODO
+});
