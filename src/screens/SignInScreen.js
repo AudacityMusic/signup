7ab30eb281fe960499efdfeced83a1ab7dfa7745
Pramad712 <pmaddineedi@gmail.com>
2024-07-01 21:13:41 -0700

    Do it on Web; Access Blocked due to Error 400: invalid_request on iOS

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import {
   StyleSheet,
   Text,
@@ -7,9 +9,72 @@ import {
   Image,
 } from "react-native";
 
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import BackButton from "../components/BackButton";
 
+WebBrowser.maybeCompleteAuthSession();
+
 export default function SignInScreen() {
+  const ANDROID_CLIENT_ID = "761199370622-jil48n0qf2v7k01td350tponk1f8digo.apps.googleusercontent.com";
+  const iOS_CLIENT_ID = "761199370622-qdq0afvq19r47p34rgjsso84leub5dlj.apps.googleusercontent.com";
+  const WEB_CLIENT_ID = "761199370622-3r5phtt6a1s6lm6554htodbk8u4k9p8f.apps.googleusercontent.com";
+
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: iOS_CLIENT_ID,
+    webClientId: WEB_CLIENT_ID,
+  });
+
+  useEffect(() => {
+    handleEffect();
+  }, [response, token]);
+
+  async function handleEffect() {
+    const user = await getLocalUser();
+    console.log("user", user);
+    if (!user) {
+      if (response?.type === "success") {
+        // setToken(response.authentication.accessToken);
+        getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(user);
+      console.log("loaded locally");
+    }
+  }
+
+  const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem("@user");
+    if (!data) return null;
+    return JSON.parse(data);
+  };
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
+  AsyncStorage.removeItem("@user");
+
   return (
     <SafeAreaView style={styles.container}>
       <Pressable>
@@ -25,12 +90,13 @@ export default function SignInScreen() {
           {"in using your Google account.\n"}
         </Text>
 
-        <Pressable style={[styles.OAuth, styles.GoogleOAuth]}>
+        <Pressable style={[styles.OAuth, styles.GoogleOAuth]} onPress={() => {promptAsync();}}>
           <Image
             style={styles.OAuthLogo}
             source={require("../assets/google-logo.png")}
           />
           <Text style={[styles.OAuthText]}> Sign in with Google</Text>
+          <Text>{JSON.stringify(userInfo, null, 2)}</Text>
         </Pressable>
       </View>
     </SafeAreaView>
