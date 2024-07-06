a3fcdec5ea1bc99d32a4801485b4c5e2753d0c74
KingPleather <59103042+KingPleather@users.noreply.github.com>
2024-07-05 21:35:57 -0700

    Update BackButton.js

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,10 +2,20 @@ import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function BackButton(props) {
   return (
-    <View style={[styles.back,props.opaque ? {backgroundColor: 'rgba(0,0,0,0.8)'} : {}]}>
+    <View
+      style={[
+        styles.back,
+        props.opaque ? { backgroundColor: "rgba(0,0,0,0.8)" } : {},
+      ]}
+    >
       <Text>{"\n\n\n"}</Text>
-      <Image source={require("./../assets/caret-left.png")} style={props.opaque ? {tintColor: "white"} : {}}/>
-      <Text style={[styles.backText, props.opaque ? {color:"white"} : {}]}>Back</Text>
+      <Image
+        source={require("./../assets/caret-left.png")}
+        style={props.opaque ? { tintColor: "white" } : {}}
+      />
+      <Text style={[styles.backText, props.opaque ? { color: "white" } : {}]}>
+        Back
+      </Text>
     </View>
   );
 }
@@ -15,16 +25,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    alignSelf:"flex-start",
-    paddingRight:"13%",
-    borderRadius:30,
-    marginVertical:"-10%"
-    
+    alignSelf: "flex-start",
+    paddingRight: "13%",
+    borderRadius: 30,
+    marginVertical: "-10%",
   },
 
   backText: {
     fontSize: 25,
-    
   },
-
 });
