67369ddfd98f98209e503716d1438d4bdd1fcc6b
Albert Young <117564829+CaptainCataIyst@users.noreply.github.com>
2024-07-06 23:03:00 -0700

    2 component changes

--- src/components/OtherOpportunities.js
+++ src/components/OtherOpportunities.js
@@ -1,55 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 
 export default function OtherOpportunities() {
-    return (
+  return (
     <View>
       <Text style={styles.header}>Other Opportunities</Text>
       <View style={styles.cardContainer}>
         <Pressable style={[styles.card, styles.first]}>
           <Image
             style={styles.icon}
-            source={require('./../assets/music-note-1.png')}
+            source={require("./../assets/music-note-1.png")}
           />
           <Text style={styles.text}>Request a Concert</Text>
           <Image
             style={styles.arrow}
-            source={require('./../assets/caret-left-1.png')}
+            source={require("./../assets/caret-left-1.png")}
           />
         </Pressable>
         <View style={styles.divider} />
         <Pressable style={[styles.card, styles.last]}>
-          <Image
-            style={styles.icon}
-            source={require('./../assets/gift.png')}
-          />
+          <Image style={styles.icon} source={require("./../assets/gift.png")} />
           <Text style={styles.text}>Make a Donation</Text>
           <Image
             style={styles.arrow}
-            source={require('./../assets/caret-left-1.png')}
+            source={require("./../assets/caret-left-1.png")}
           />
         </Pressable>
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-
   header: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   cardContainer: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 10,
   },
   card: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     padding: 10,
-    borderColor: '#e0e0e0',
+    borderColor: "#e0e0e0",
     borderWidth: 1,
   },
   first: {
@@ -68,7 +64,7 @@ const styles = StyleSheet.create({
   text: {
     flex: 1,
     fontSize: 14,
-    color: '#555',
+    color: "#555",
   },
   arrow: {
     width: 20,
@@ -77,7 +73,6 @@ const styles = StyleSheet.create({
   },
   divider: {
     height: 0.1,
-    backgroundColor: '#e0e0e0',
+    backgroundColor: "#e0e0e0",
   },
 });
-
