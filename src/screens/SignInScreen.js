348fabc25a2e33e23a94190c4751635880782454
Pramad712 <pmaddineedi@gmail.com>
2024-07-07 16:18:58 -0700

    formatting

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -5,12 +5,10 @@ import {
   SafeAreaView,
   Pressable,
   Image,
-  AppRegistry,
 } from "react-native";
 
 import {
   GoogleSignin,
-  GoogleSigninButton,
   statusCodes,
   isErrorWithCode,
 } from "@react-native-google-signin/google-signin";
@@ -18,9 +16,11 @@ import {
 import BackButton from "../components/BackButton";
 
 GoogleSignin.configure({
-  webClientId: "761199370622-3r5phtt6a1s6lm6554htodbk8u4k9p8f.apps.googleusercontent.com", // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+  webClientId:
+    "761199370622-3r5phtt6a1s6lm6554htodbk8u4k9p8f.apps.googleusercontent.com", // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
   scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
-  iosClientId: "761199370622-qdq0afvq19r47p34rgjsso84leub5dlj.apps.googleusercontent.com", // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  iosClientId:
+    "761199370622-qdq0afvq19r47p34rgjsso84leub5dlj.apps.googleusercontent.com", // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
 });
 
 export default function SignInScreen() {
@@ -39,51 +39,8 @@ export default function SignInScreen() {
           {"in using your Google account.\n"}
         </Text>
 
-        <Pressable style={[styles.OAuth, styles.GoogleOAuth]} 
-        onPress={async () => {
-          try {
-              await GoogleSignin.hasPlayServices();
-              const userInfo = await GoogleSignin.signIn();
-              console.log("success");
-              console.log(JSON.stringify(userInfo, null, 2));
-            } catch (error) {
-              if (isErrorWithCode(error)) {
-                switch (error.code) {
-                  case statusCodes.SIGN_IN_CANCELLED:
-                    // user cancelled the login flow
-                    console.log("Cancelled");
-                    break;
-                  case statusCodes.IN_PROGRESS:
-                    // operation (eg. sign in) already in progress
-                    console.log("In progress");
-                    break;
-                  case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-                    // play services not available or outdated
-                    console.log("Play services not available or outdated");
-                    break;
-                  case statusCodes.SIGN_IN_REQUIRED:
-                  console.log("Sign in required")
-                    break;
-                  default:
-                    console.log(error);
-                    break;
-                  // some other error happened
-                }
-              } else {
-                console.log("WOAH This is outta my control");
-                // an error that's not related to google sign in occurred
-              }
-            }
-          }}>
-          <Image
-            style={styles.OAuthLogo}
-            source={require("../assets/google.png")}
-          />
-          <Text style={[styles.OAuthText]}> Sign in with Google</Text>
-        </Pressable>
-        {/* <GoogleSigninButton 
-          size={GoogleSigninButton.Size.Wide}
-          color={GoogleSigninButton.Color.Light}
+        <Pressable
+          style={[styles.OAuth, styles.GoogleOAuth]}
           onPress={async () => {
             try {
               await GoogleSignin.hasPlayServices();
@@ -106,7 +63,7 @@ export default function SignInScreen() {
                     console.log("Play services not available or outdated");
                     break;
                   case statusCodes.SIGN_IN_REQUIRED:
-                  console.log("Sign in required")
+                    console.log("Sign in required");
                     break;
                   default:
                     console.log(error);
@@ -119,7 +76,13 @@ export default function SignInScreen() {
               }
             }
           }}
-        /> */}
+        >
+          <Image
+            style={styles.OAuthLogo}
+            source={require("../assets/google.png")}
+          />
+          <Text style={[styles.OAuthText]}> Sign in with Google</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
