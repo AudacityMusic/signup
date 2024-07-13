e544ddcd38aece2cbde4e0da5b8dadf858b3e7e9
Pramad712 <pmaddineedi@gmail.com>
2024-07-12 17:46:44 -0700

    small phone capability

--- src/screens/OtherInfoScreen.js
+++ src/screens/OtherInfoScreen.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Pressable,
   View,
@@ -6,6 +7,9 @@ import {
   Text,
   Image,
   ImageBackground,
+  KeyboardAvoidingView,
+  Dimensions,
+  ScrollView
 } from "react-native";
 import NextButton from "../components/NextButton";
 import BackButton from "../components/BackButton";
@@ -15,60 +19,68 @@ import UploadButton from "../components/UploadButton";
 import TextField from "../components/TextField";
 
 export default function OtherInfoScreen() {
+  const [otherInfo, setOtherInfo] = useState("");
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.checkBoxesContainer}>
-        <View
-          style={{
-            paddingLeft: "4%",
-            paddingRight: "20%",
-            flex: 1,
-            justifyContent: "space-between",
-            paddingTop: "2%",
-            flexDirection: "column",
-          }}
-        >
-          <CheckBoxQuery
-            question={
-              "Do you give permission for Audacity Music Club to post recordings of your performance on public websites? "
-            }
-          />
-          <CheckBoxQuery
-            question={
-              "My parent has given their consent for my participation. "
-            }
-          />
-        </View>
-      </View>
-      <View style={styles.uploadsContainer}>
-        <View>
-          <Text style={{ paddingBottom: "3%" }}>
-            Our volunteer piano accompanist can provide sight reading
-            accompaniment for entry level players. To request this service,
-            upload the main score AND accompaniment score in one PDF file. (100
-            MB file size limit)
-          </Text>
-          <UploadButton />
-        </View>
-        <View style={{ paddingTop: "3%" }}>
-          <Text style={{ paddingBottom: "3%" }}>
-            Upload your Library Band Ensemble profile as one PDF file.
-          </Text>
-          <UploadButton />
-        </View>
-      </View>
-      <View style={styles.otherInfoContainer}>
-        <TextField
-          title={
-            "Other Information, such as special requests in sequence arrangement (optional) "
-          }
-        />
-      </View>
-      <View style={styles.nextContainer}>
-        <Pressable style={[{ marginBottom: "5%", marginRight: "4%" }]}>
-          <NextButton />
-        </Pressable>
-      </View>
+      <KeyboardAvoidingView style={{height: Dimensions.get("window").height}} behavior="padding">
+        <ScrollView>
+          <View style={styles.checkBoxesContainer}>
+            <View
+              style={{
+                paddingLeft: "4%",
+                paddingRight: "20%",
+                flex: 1,
+                justifyContent: "space-between",
+                paddingTop: "2%",
+                flexDirection: "column",
+              }}
+            >
+              <CheckBoxQuery
+                question={
+                  "Do you give permission for Audacity Music Club to post recordings of your performance on public websites? "
+                }
+              />
+              <CheckBoxQuery
+                question={
+                  "My parent have given their consent for my participation. "
+                }
+              />
+            </View>
+          </View>
+          <View style={styles.uploadsContainer}>
+            <View>
+              <Text style={{ paddingBottom: "3%" }}>
+                Our volunteer piano accompanist can provide sight reading
+                accompaniment for entry level players. To request this service,
+                upload the main score AND accompaniment score in one PDF file. (100
+                MB file size limit)
+              </Text>
+              <UploadButton />
+            </View>
+            <View style={{ paddingTop: "3%" }}>
+              <Text style={{ paddingBottom: "3%" }}>
+                Upload your Library Band Ensemble profile as one PDF file.
+              </Text>
+              <UploadButton />
+            </View>
+          </View>
+          <View style={styles.otherInfoContainer}>
+            <TextField
+              title={
+                "\nOther Information, such as special requests in sequence arrangement (optional) "
+              }
+              setText={(text) => {setOtherInfo(text)}}
+              keyboardType="default"
+            />
+          </View>
+          <View style={styles.nextContainer}>
+            <Pressable style={[{ marginBottom: "5%", marginRight: "4%" }]}>
+              <NextButton />
+            </Pressable>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
