84b81842cd727224e51669541053a55c412d38ca
KingPleather <59103042+KingPleather@users.noreply.github.com>
2024-07-20 16:03:55 -0700

    made carousel work

--- src/components/CarouselPage.js
+++ src/components/CarouselPage.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import colors from "../constants/colors";
 import VolunteerOpportunity from "./VolunteerOpportunity";
-import Carousel from "react-native-snap-carousel";
+import Carousel from "react-native-reanimated-carousel";
 
 
 
@@ -9,15 +9,24 @@ export default function CarouselPage({ navigation,data }) {
   const renderItem =({item})=> {
     return (
       <View>
-        <VolunteerOpportunity navigation={navigation} title={item.title} location={item.location} time={item.time} />
+        <VolunteerOpportunity navigation={navigation} title={item[0].title} location={item[0].location} time={item[0].time} />
+        <VolunteerOpportunity navigation={navigation} title={item[1].title} location={item[1].location} time={item[1].time} />
+        <VolunteerOpportunity navigation={navigation} title={item[2].title} location={item[2].location} time={item[2].time} />
+
       </View>
     );
   }
   return (
-    <Carousel
-      data={data}
-      renderItem={renderItem}
-    />
+    <View>
+      <Carousel
+        width={400}
+        height={375}
+        data={data}
+        renderItem={renderItem}
+      />
+      <Text>Hi</Text>
+    </View>
+    
   );
 }
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,22 +3,41 @@ import VolunteerOpportunity from "../components/VolunteerOpportunity";
 import OtherOpportunities from "../components/OtherOpportunities";
 import Heading from "../components/Heading";
 import Websites from "../components/Websites";
-import Carousel from "react-native-snap-carousel";
 import CarouselPage from "../components/CarouselPage";
 
 export default function HomeScreen({ navigation }) {
+
+  function formatData(data) {
+    let count = 0;
+    let formattedArray = [];
+    let tempArray = [];
+    for(let i = 0; i < data.length; i++) {
+        tempArray.push(data[i]);
+        if (tempArray.length === 3) {
+            formattedArray.push(tempArray);
+            tempArray = [];
+        }
+    }
+    return formattedArray;
+  }
   const testData = [
-    {title: "Library Music Hour", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
-    {title: "Library Music Hour", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
-    {title: "Library Music Hour", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
-    
+
+      {title: "Library Music Hour1", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour2", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour3", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour4", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour5", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour6", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour7", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour8", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"},
+      {title: "Library Music Hour9", location: "Fremont Main Libary", time: "Saturday, August 10, 2024 2:30 PM", imagePath: "./../assets/warm-springs-bart.png"}
   
   ];
 
   return (
     <ScrollView style={styles.container}>
       <Heading>Volunteer Opportunities</Heading>
-      <CarouselPage navigation={navigation} data={testData}/>
+      <CarouselPage navigation={navigation} data={formatData(testData)}/>
       <Heading>Other Opportunities</Heading>
       <OtherOpportunities />
       <Heading>Websites</Heading>
