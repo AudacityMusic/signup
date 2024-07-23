73def508f5439734a6c278010e165130aae62bf6
TenType <55125103+TenType@users.noreply.github.com>
2024-07-22 21:00:22 -0700

    Fix EAS Update dynamic config

--- app.config.js
+++ app.config.js
@@ -29,6 +29,12 @@ export default {
       projectId: "56bb99cb-9cbe-423d-9a88-3f82a0cf3aa0",
     },
   },
+  updates: {
+    url: "https://u.expo.dev/56bb99cb-9cbe-423d-9a88-3f82a0cf3aa0",
+  },
+  runtimeVersion: {
+    policy: "appVersion",
+  },
   plugins: [
     [
       "@react-native-google-signin/google-signin",
