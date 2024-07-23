e7906d37f2ec8bed28d7dca04bdccbf3122e4391
Pramad712 <pmaddineedi@gmail.com>
2024-07-23 10:31:41 -0700

    use map (refractoring)

--- src/components/CarouselPage.js
+++ src/components/CarouselPage.js
@@ -7,37 +7,20 @@ export default function CarouselPage({ navigation, data }) {
   const renderItem = ({ item }) => {
     return (
       <View>
-        <VolunteerOpportunity
-          navigation={navigation}
-          title={item[0].Title}
-          location={item[0].Location}
-          date={item[0].Date}
-          image={item[0].Image}
-          description={item[0].Description}
-          tags={item[0].Tags.split(", ")}
-        />
-        {item.length > 1 ? (
-          <VolunteerOpportunity
-            navigation={navigation}
-            title={item[1].Title}
-            location={item[1].Location}
-            date={item[1].Date}
-            image={item[1].Image}
-            description={item[1].Description}
-            tags={item[1].Tags.split(", ")}
-          />
-        ) : null}
-        {item.length > 2 ? (
-          <VolunteerOpportunity
-            navigation={navigation}
-            title={item[2].Title}
-            location={item[2].Location}
-            date={item[2].Date}
-            image={item[2].Image}
-            description={item[2].Description}
-            tags={item[2].Tags.split(", ")}
-          />
-        ) : null}
+        {item.map((component, index) => {
+          return (
+            <VolunteerOpportunity
+              navigation={navigation}
+              title={component.Title}
+              location={component.Location}
+              date={component.Date}
+              image={component.Image}
+              description={component.Description}
+              tags={component.Tags.split(", ")}
+              key={index}
+            />
+          );
+        })}
       </View>
     );
   };
