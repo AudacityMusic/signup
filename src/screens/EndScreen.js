3188eafed1121b00f590f1f8fbadb2919e437397
KingPleather <59103042+KingPleather@users.noreply.github.com>
2024-07-27 19:57:34 -0700

    fixed prettier issues

--- src/screens/EndScreen.js
+++ src/screens/EndScreen.js
@@ -1,12 +1,28 @@
 import { View, Image, Text } from "react-native";
-import Ionicons from '@expo/vector-icons/Ionicons';
-
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function AccountScreen({ navigation, success }) {
   return (
-    <View style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center",flex:1}}>
-      <Ionicons name={success ? "checkmark-circle" : "close-circle"} size={144} color={success ? "green" : "red"} style={{marginBottom:"15%"}}/>
-      <Text style={{justifyContent:"center", fontSize:24}}>{success ? "Form submitted successfully" : "Error: Form submission was unsuccessful, try again later."}</Text>
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+      }}
+    >
+      <Ionicons
+        name={success ? "checkmark-circle" : "close-circle"}
+        size={144}
+        color={success ? "green" : "red"}
+        style={{ marginBottom: "15%" }}
+      />
+      <Text style={{ justifyContent: "center", fontSize: 24 }}>
+        {success
+          ? "Form submitted successfully"
+          : "Error: Form submission was unsuccessful, try again later."}
+      </Text>
     </View>
   );
 }
