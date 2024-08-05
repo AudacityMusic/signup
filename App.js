e27965eac23a4aba4db8f5f561afe8d75fc741bc
AZ <alex.hc.zhang@gmail.com>
2024-08-04 23:40:29 -0700

    Fixed issues
    I think the issues should be gone.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import "@expo/metro-runtime";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, View, Text, SafeAreaView } from "react-native";
+import { ActivityIndicator, SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
