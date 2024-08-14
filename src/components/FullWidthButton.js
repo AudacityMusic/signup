6380ff561827c6a2aeb2bd3b63a153193ffc8c71
TenType <55125103+TenType@users.noreply.github.com>
2024-08-13 19:47:17 -0700

    Add clear data button

---
+++ src/components/FullWidthButton.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, Pressable } from "react-native";
+
+export default function FullWidthButton({
+  buttonStyle,
+  textStyle,
+  onPress,
+  children,
+}) {
+  return (
+    <Pressable style={[buttonStyle, styles.button]} onPress={onPress}>
+      <Text style={[textStyle, styles.text]}>{children}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 10,
+    marginVertical: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 110,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "95%",
+    alignSelf: "center",
+  },
+  text: {
+    fontSize: 18,
+  },
+});
