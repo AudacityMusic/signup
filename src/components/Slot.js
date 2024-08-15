ddb0d5d16847898e08d05ad7eb0839fbe30b0868
Pramad712 <pmaddineedi@gmail.com>
2024-08-15 16:05:09 -0700

    add time slot component

---
+++ src/components/Slot.js
@@ -0,0 +1,162 @@
+import { useState } from 'react';
+import { View, Pressable, Text, StyleSheet } from 'react-native'
+import DatePicker from 'react-native-date-picker'
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+function timeFormatter(date) {
+    const hour = date.getHours() % 12 == 0 ? 12 : date.getHours() % 12;
+    const minute = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+    const period = date.getHours() >= 12 ? "PM" : "AM";
+    return `${hour}:${minute} ${period}`;
+}
+
+function dateFormatter(date) {
+    const days = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+    ];
+    return `${days[date.getDay()]} ${date.getMonth() + 1}/${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}/${date.getFullYear() - 2000} ${timeFormatter(date)}`;
+}
+
+export class TimeSlot {
+    constructor(start=new Date(), end=new Date()) {
+        this.start = start;
+        this.end = end;
+    }
+
+    toString() {
+        return `${dateFormatter(this.start)} - ${timeFormatter(this.end)}`;
+    }
+
+    compareTo(other) {
+        if (this.start < other.start) {
+        return -1;
+        } else if (this.start > other.start) {
+        return 1;
+        } else {
+            if (this.end < other.end) {
+                return -1;
+            } else if (this.end > other.end) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    render(list, setList, index, setIndex, setOpen, setAdded, setStart) {
+        return (
+            <View key={index * 10} style={{flexDirection: "row", justifyContent: "space-between"}}>
+                <View key={index * 10 + 1} style={{flexDirection: "row"}}>
+                    <Pressable key={index * 10 + 2} onPress={() => {setOpen(true); setAdded(false); setStart(true); setIndex(index);}}>
+                        <Text key={index * 10 + 3} style={{fontSize: 20, textDecorationLine: "underline"}}>{dateFormatter(this.start)}</Text>
+                    </Pressable>
+                    <Text key={index * 10 + 4} style={{fontSize: 20}}> - </Text>
+                    <Pressable key={index * 10 + 5} onPress={() => {setOpen(true); setAdded(false); setStart(false); setIndex(index);}}>
+                        <Text key={index * 10 + 6} style={{fontSize: 20, textDecorationLine: "underline"}}>{timeFormatter(this.end)}</Text>
+                    </Pressable>
+                </View>
+                <RemoveButton list={list} setList={setList} index={index} />
+            </View>
+        );
+    }
+}
+
+function RemoveButton({list, setList, index}) {
+    return (
+        <Pressable key={index * 10 + 7} style={styles.button} onPress={() => {setList(previous => {return previous.slice(0, index).concat(previous.slice(index + 1, list.length))})}}>
+            <Ionicons key={index * 10 + 8} name="remove-circle-sharp" size={23} color="#FF3B30" />
+            <Text key={index * 10 + 9} style={{color: "#FF3B30", fontSize: 20}}>  Delete</Text>
+        </Pressable>
+    );
+}
+
+function AddButton({list, setList, setIndex, setAdded, setOpen, setStart}) {
+    return (
+        <Pressable style={styles.button} onPress={() => {
+            setAdded(true); 
+            setStart(true);
+            let length = list.length;
+            setIndex(length);
+            setList((previous) => previous.concat([new TimeSlot()]));
+            setOpen(true);
+        }}>
+            <Ionicons name="add-circle-sharp" size={28} color="#006AFF" />
+            <Text style={{color: "#006AFF", fontSize: 25}}>  Add Time Slot</Text>
+        </Pressable>
+    );
+}
+
+export function Select({list, setList, index, added, start, setStart, open, setOpen}) {
+    const now = new Date();
+    console.log(list);
+
+    return (
+        <DatePicker
+            modal
+            open={open}
+            date={new Date()}
+            mode={start ? "datetime" : "time"}
+            title = {start ? "Select Start Time" : "Select End Time"}
+            minimumDate={start ? new Date() : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0)}
+            maximumDate={start ? new Date(now.getFullYear() + 1, now.getMonth(), now.getDate(), 23, 59) : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59)}
+            onConfirm={(date) => {
+                if (start) {
+                    setList((previous) => {let next = previous; next[index].start = date; return next});
+                    setStart(false);
+
+                    setOpen(false);
+                    
+                    if (added) {
+                        setOpen(true);
+                    }
+                }
+
+                else {
+                    setList((previous) => {let next = previous; next[index].end = date; return next})
+                    setOpen(false);
+                }
+            }}
+
+            onCancel={() => {
+                if (added && start) {
+                    setList((previous) => {return previous.slice(0, index - 1)});
+                }
+                
+                setOpen(false)
+            }}
+        />
+    );
+}
+
+export default function SlotList({slots, setSlots}) {
+    const [open, setOpen] = useState(false);
+    const [start, setStart] = useState(true);
+    const [added, setAdded] = useState(false);
+    const [index, setIndex] = useState(0);
+  
+    return (
+      <View style={styles.container}>
+        {slots.map((slot, index) => (slot == null) || (((index == slots.length - 1) && open && added)) ? null : slot.render(slots, setSlots, index, setIndex, setOpen, setAdded, setStart))}
+        <AddButton list={slots} setList={setSlots} setIndex={setIndex} setAdded={setAdded} setOpen={setOpen} setStart={setStart} />
+        <Select list={slots} setList={setSlots} index={index} added={added} start={start} setStart={setStart} open={open} setOpen={setOpen} />
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 50
+    },
+
+    button: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+});
