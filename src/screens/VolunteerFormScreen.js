93e34d6267a4ebc7fda693657684763312c815f3
Pranav Maddineedi <pmaddineedi@gmail.com>
2026-05-24 18:09:23 -0700

    done

--- src/screens/VolunteerFormScreen.js
+++ src/screens/VolunteerFormScreen.js
@@ -17,6 +17,7 @@ import {
   Text,
   View,
 } from "react-native";
+import Markdown from "react-native-markdown-display";
 import WebView from "react-native-webview";
 import Fuse from "fuse.js";
 import ErrorBoundary from "react-native-error-boundary";
@@ -24,7 +25,7 @@ import ErrorBoundary from "react-native-error-boundary";
 import NextButton from "../components/NextButton";
 import PersistScrollView from "../components/PersistScrollView";
 
-import { alertError, sendErrorEmail, openInMaps } from "../utils";
+import { openURL, sendErrorEmail } from "../utils";
 import DanceClub from "../utils/forms/DanceClub";
 import LibraryMusicHour from "../utils/forms/LibraryMusicHour";
 import MusicByTheTracks from "../utils/forms/MusicByTheTracks";
@@ -160,14 +161,21 @@ export default function VolunteerFormScreen({ navigation, route }) {
                   </Text>
                 )}
                 {location == null ? null : (
-                  <Pressable onPress={() => openInMaps(location)}>
-                    <Text
-                      style={[styles.headerText, styles.locationText]}
-                      selectable={true}
+                  <View style={{ alignItems: "center" }}>
+                    <Markdown
+                      onLinkPress={(url) => {
+                        openURL(url);
+                        return false;
+                      }}
+                      style={{
+                        body: {
+                          fontSize: 18,
+                        },
+                      }}
                     >
                       {location}
-                    </Text>
-                  </Pressable>
+                    </Markdown>
+                  </View>
                 )}
               </View>
               {/* Render each question component from form */}
